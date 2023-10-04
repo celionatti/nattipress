2de04c229559a219acda7452dca18bcbc1a818ce
celionatti <amisuusman@gmail.com>
2023-10-04 07:51:20 -0700

    Day 4 v2

---
+++ NattiCore/Bolt/init.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+require_once FCPATH . 'config/np_load.php';
+require_once FCPATH . 'NattiCore/Database/Database.php';
+require_once FCPATH . 'NattiCore/Database/Migration/Migration.php';
+require_once FCPATH . 'NattiCore/Bolt/bolt.php';
---
+++ NattiCore/Bolt/samples/controller-sample.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+defined('FCPATH') or die("Direct script access denied");
+
+namespace NattiThemes\np_{NAMESPACE}\models;
+
+/**put the controller code for your plugin here**/
